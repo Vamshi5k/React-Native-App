fd3167c4d70bde75ad3f7f9c7489756f02ec9301
vamshi <vamshianimela.3@gmail.com>
2024-02-14 18:31:16 +0530

    Second Version

--- app/index.js
+++ app/index.js
@@ -1,12 +1,26 @@
-import { useState } from "react"; 
-import { View , ScrollView, SafeAreaView } from 'react-native';
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView } from "react-native";
 import { Slot, useRouter } from "expo-router";
 import { Text } from "react-native";
-import {COLORS, icons,images, SIZES} from "../constants"
-import { Nearbyjobs} from '../components/home/nearby/Nearbyjobs';
-import {Popularjobs} from '../components/home/popular/Popularjobs'
+import { COLORS, icons, images, SIZES } from "../constants";
+import { Nearbyjobs } from "../components/home/nearby/Nearbyjobs";
+import { Popularjobs } from "../components/home/popular/Popularjobs";
+import { ScreenHeaderBtn } from "../components/common/header/ScreenHeaderBtn";
+import { Welcome } from "../components/home/welcome/Welcome";
+
+const Home = () => {
+  const router = useRouter();
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Slot.Screen
+         options={{
+            headerStyle: {backgroundColor: COLORS.lightWhite},
+         }}
+      />
+    </SafeAreaView>
+  );
+};
 
 
-export default function Page() {
-  return <Text>Home page hey</Text>;
-}
+export default Home;
