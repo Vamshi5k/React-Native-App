f3fa0656ae9d6a6a8aa434a973c8f57c4e838469
vamshi <vamshianimela.3@gmail.com>
2024-02-14 18:14:39 +0530

    Added Files

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { useState } from "react"; 
+import { View , ScrollView, SafeAreaView } from 'react-native';
+import { Slot, useRouter } from "expo-router";
+import { Text } from "react-native";
+import {COLORS, icons,images, SIZES} from "../constants"
+import { Nearbyjobs} from '../components/home/nearby/Nearbyjobs';
+import {Popularjobs} from '../components/home/popular/Popularjobs'
+
+
+export default function Page() {
+  return <Text>Home page hey</Text>;
+}
